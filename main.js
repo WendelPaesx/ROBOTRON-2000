d42fba42664c1736d787cdbd72f241eb6c01ad9f
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-10-18 03:16:40 -0300

    .

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+const estatistica = document.querySelectorAll("[data-estatistica]")
+console.log(estatistica)
 
 
 const controle = document.querySelectorAll("[data-controle]")
 
-controle.forEach((elemento) =>{
-    elemento.addEventListener("click", (evento) =>{
-        manipulaDados(evento.target.dataset.controle,evento.target.parentNode)
-       
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
+
     })
 }
 )
@@ -18,12 +50,23 @@ controle.forEach((elemento) =>{
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
-    if(operacao=== "-"){
+    if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1
-    }else{
+    } else {
         peca.value = parseInt(peca.value) + 1
 
     }
 }
 
-dizOi("Pedro")
+function atualizaEstatisticas(peca) {
+    console.log()
+
+    estatistica.forEach((elemento)=>{
+        
+        
+       elemento.textContent = parseInt(elemento.textContent)+ pecas[peca][elemento.dataset.estatistica]
+        
+
+    })
+
+}
